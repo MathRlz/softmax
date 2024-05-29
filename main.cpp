615a4b256bd160df875582643e3ecbe235edde50
Krystian Chmielewski <krychatko@gmail.com>
2024-05-29 23:45:23 +0200

    initial opencl impl

--- main.cpp
+++ main.cpp
@@ -5,32 +5,109 @@
 #include <vector>
 #include <cassert>
 #include <exception>
+#include <functional>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <numeric>
 
-const char* kernelSource = R"(
-__kernel void vector_add(__global const float* A, __global const float* B, __global float* C, int n) {
-    int id = get_global_id(0);
-    if (id < n) {
-        C[id] = A[id] + B[id];
+using namespace std;
+
+string readFile(const string &fileName) {
+    ifstream file(fileName);
+    return string(istreambuf_iterator<char>(file), {});
+}
+
+void printArray(const vector<float> &arr, const vector<size_t> &dims) {
+    vector<size_t> dimCntrs;
+    size_t dimCntr=1;
+    for (int i = dims.size()-1; i >= 0; i--) {
+        dimCntr *= dims[i];
+        dimCntrs.push_back(dimCntr);
+    }
+
+
+    vector<size_t> mods(dims.size(), 0);
+    for (size_t i = 0; i < arr.size(); i++) {
+        for (size_t j = 0; j < dimCntrs.size(); j++) {
+            mods[j] = i % dimCntrs[j];
+
+            if (mods[j] == 0) {
+                cout << "[";
+            }
+        }
+
+
+        cout << arr[i];
+
+        bool needEndl = false;
+        for (size_t j = 0; j < dimCntrs.size(); j++) {
+            if (mods[j] == dimCntrs[j] - 1) {
+                cout << "]";
+                needEndl = true;
+            }
+        }
+
+        cout << ", ";
+        if(needEndl) cout << endl;
     }
 }
 
-__kernel void max(__global const float *arr, __global const int* shape, int dims, __global const int *axis, int numAxis) {
-
+vector<size_t> getDims(const vector<size_t> &oldDims, const vector<int> &axes) {
+    vector<size_t> newDims(oldDims.size());
+    for (size_t i = 0; i < oldDims.size(); i++) {
+        if (find(axes.begin(), axes.end(), i) == end(axes)) {
+            newDims[i] = oldDims[i];
+        } else {
+            newDims[i] = 1;
+        }
+    }
+    return newDims;
 }
 
-__kernel void max1d(__global const float *arr, )
-__kernel void maxOverAxis(__global const float *arr, __global const int *shape, int dims, int axis) {
-
+vector<size_t> getDimCntr(const vector<size_t> &dims) {
+    vector<size_t> dimCntr(dims.size());
+    size_t tmp = 1;
+    for (int i = dims.size()-1; i >= 0; i--) {
+        dimCntr[i] = tmp;
+        tmp *= dims[i];
+    }
+    return dimCntr;
 }
-)";
 
 int main() {
     // Initialize data
     const int N = 1056;
-    std::vector<float> A(N, 1.0f); // Vector A with all elements 1.0
-    std::vector<float> B(N, 2.0f); // Vector B with all elements 2.0
-    std::vector<float> C(N, 0.0f); // Result vector C
+    
+    const vector<size_t> dims = {2, 3, 3};
+    size_t size = accumulate(dims.begin(), dims.end(), 1, multiplies<size_t>());
 
+    vector<float> array = { 1, 2, 3,
+                        4, 5, 6,
+                        7, 8, 9,
+                      
+                        10, 11, 12,
+                     13, 14, 15,
+                     16, 17, 18};
+
+    //srand(unsigned(time(nullptr)));
+    //generate(array.begin(), array.end(), rand);
+
+    cout.precision(8);
+    cout << scientific;
+
+    cout << "Array:\n";
+    printArray(array, dims);
+    vector<float> resultArray(size, 0.0f);
+
+    const vector<int> axes = {1};
+    const auto reducedDims = getDims(dims, axes);
+    size_t reducedSize = accumulate(reducedDims.begin(), reducedDims.end(), 1, multiplies<size_t>());
+
+    auto dimCntr = getDimCntr(dims);
+    auto valDimsCntr = getDimCntr(reducedDims);
+
+    
     try {
         // Get all platforms (drivers)
         std::vector<cl::Platform> platforms;
@@ -47,36 +124,88 @@ int main() {
         cl::CommandQueue queue(context, devices[0]);
 
         // Allocate device memory and transfer input data to device
-        cl::Buffer bufferA(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(float) * N, A.data());
-        cl::Buffer bufferB(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(float) * N, B.data());
-        cl::Buffer bufferC(context, CL_MEM_WRITE_ONLY, sizeof(float) * N);
+        cl::Buffer arrBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(float) * size, array.data());
+        cl::Buffer reductionBuffer(context, CL_MEM_READ_WRITE, reducedSize * sizeof(float));
+        cl::Buffer dimsBuffer(context, dims.begin(), dims.end(), true);
+        cl::Buffer reducedDimBuffer(context, reducedDims.begin(), reducedDims.end(), true);
+        cl::Buffer dimCntrBuffer(context, dimCntr.begin(), dimCntr.end(), true);
+        cl::Buffer valDimsCntrBuffer(context, valDimsCntr.begin(), valDimsCntr.end(), true);
+        int numDims = static_cast<int>(dims.size());
+
 
         // Create and build the program
         cl::Program::Sources sources;
-        sources.push_back({kernelSource, strlen(kernelSource)});
+        sources.push_back(readFile("softmax.cl"));
         cl::Program program(context, sources);
-        program.build(devices);
+        try {
+            program.build(devices);
+        } catch (const cl::Error& err) {
+            if (err.err() == CL_BUILD_PROGRAM_FAILURE) {
+                for (auto& device : devices) {
+                    std::string buildLog = program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(device);
+                    std::cerr << "Build log for device " << device.getInfo<CL_DEVICE_NAME>() << ":\n";
+                    std::cerr << buildLog << "\n";
+                }
+            }
+            return -1;
+        }
 
         // Create the kernel
-        cl::Kernel kernel(program, "vector_add");
-
+        cl::Kernel maxKernel(program, "max");
         // Set the kernel arguments
-        kernel.setArg(0, bufferA);
-        kernel.setArg(1, bufferB);
-        kernel.setArg(2, bufferC);
-        kernel.setArg(3, N);
+        maxKernel.setArg(0, arrBuffer);
+        maxKernel.setArg(1, dimsBuffer);
+        maxKernel.setArg(2, reductionBuffer);
+        maxKernel.setArg(3, numDims);
+        maxKernel.setArg(4, axes[0]);
+
+        cl::NDRange globalReduce(reducedSize);
+        queue.enqueueNDRangeKernel(maxKernel, cl::NullRange, globalReduce, cl::NullRange);
+        
+        vector<float> maxValues(reducedSize);
+        queue.enqueueReadBuffer(reductionBuffer, CL_TRUE, 0, sizeof(float) * reducedSize, maxValues.data());
+        cout << "Max:\n";
+        printArray(maxValues, reducedDims);
+
+
+        cl::Kernel subAndExpKernel(program, "sub_and_exp");
+        subAndExpKernel.setArg(0, arrBuffer);
+        subAndExpKernel.setArg(1, dimsBuffer);
+        subAndExpKernel.setArg(2, reductionBuffer);
+        subAndExpKernel.setArg(3, reducedDimBuffer);
+        subAndExpKernel.setArg(4, dimCntrBuffer);
+        subAndExpKernel.setArg(5, valDimsCntrBuffer);
+        subAndExpKernel.setArg(6, numDims);
+
+        cl::NDRange global(size);
+        queue.enqueueNDRangeKernel(subAndExpKernel, cl::NullRange, global, cl::NullRange);
+
+        cl::Kernel sumKernel(program, "sum");
+        sumKernel.setArg(0, arrBuffer);
+        sumKernel.setArg(1, dimsBuffer);
+        sumKernel.setArg(2, reductionBuffer);
+        sumKernel.setArg(3, numDims);
+        sumKernel.setArg(4, axes[0]);
+        queue.enqueueNDRangeKernel(sumKernel, cl::NullRange, globalReduce, cl::NullRange);
+
+        cl::Kernel divKernel(program, "div");
+        divKernel.setArg(0, arrBuffer);
+        divKernel.setArg(1, dimsBuffer);
+        divKernel.setArg(2, reductionBuffer);
+        divKernel.setArg(3, reducedDimBuffer);
+        divKernel.setArg(4, dimCntrBuffer);
+        divKernel.setArg(5, valDimsCntrBuffer);
+        divKernel.setArg(6, numDims);
+        queue.enqueueNDRangeKernel(divKernel, cl::NullRange, global, cl::NullRange);
+
 
         // Execute the kernel
-        cl::NDRange global(N);
-        queue.enqueueNDRangeKernel(kernel, cl::NullRange, global, cl::NullRange);
 
         // Read back the result from the device to host
-        queue.enqueueReadBuffer(bufferC, CL_TRUE, 0, sizeof(float) * N, C.data());
+        queue.enqueueReadBuffer(arrBuffer, CL_TRUE, 0, sizeof(float) * size, array.data());
+        cout << "Softmax:\n";
+        printArray(array, dims);
 
-        // Print the results
-        for (int i = 1024; i < 1056; ++i) { // Print first 10 elements for verification
-            std::cout << C[i] << " ";
-        }
         std::cout << std::endl;
 
     } catch (cl::Error& err) {
---
+++ softmax.cl
@@ -0,0 +1,98 @@
+__kernel void vector_add(__global const float* A, __global const float* B, __global float* C, int n) {
+    int id = get_global_id(0);
+    if (id < n) {
+        C[id] = A[id] + B[id];
+    }
+}
+
+__kernel void max(__global const float *arr, __global const size_t *dims, __global float *results, int numDims, int axis) {
+    int id = get_global_id(0);
+    
+    ulong size = 1;
+    for (int i = 0; i < numDims; i++) {
+        if (i != axis) {
+            size *= dims[i];
+        }
+    }
+    if (id > size) {
+        return;
+    }
+
+    ulong stride = 1;
+    for (int i = numDims - 1; i > axis; i--) {
+        stride *= dims[i];
+    }
+
+    size_t dimSizesUpTo = stride * dims[axis];
+    size_t pos = (id / stride) * dimSizesUpTo + id % stride;
+
+    float maxVal = FLT_MIN;
+    for (size_t j = 0; j < dims[axis]; j++) {
+        size_t offset = pos + j * stride;
+        float val = arr[offset];
+        if (val > maxVal) {
+            maxVal = val;
+        }
+    }
+    results[id] = maxVal;
+}
+
+__kernel void sum(__global const float *arr, __global const size_t *dims, __global float *results, int numDims, int axis) {
+    int id = get_global_id(0);
+    
+    ulong size = 1;
+    for (int i = 0; i < numDims; i++) {
+        if (i != axis) {
+            size *= dims[i];
+        }
+    }
+    if (id > size) {
+        return;
+    }
+
+    ulong stride = 1;
+    for (int i = numDims - 1; i > axis; i--) {
+        stride *= dims[i];
+    }
+
+    size_t dimSizesUpTo = stride * dims[axis];
+    size_t pos = (id / stride) * dimSizesUpTo + id % stride;
+
+    float sum = 0.0f;
+    for (size_t j = 0; j < dims[axis]; j++) {
+        size_t offset = pos + j * stride;
+        float val = arr[offset];
+        sum += val;
+    }
+    results[id] = sum;
+}
+
+__kernel void sub_and_exp(__global float *arr, __global const size_t *dims,
+                  __global const float *vals, __global const size_t *valDims,
+                  __global const size_t *dimCntr, __global const size_t *valDimsCntr,
+                  int numDims) {
+    int id = get_global_id(0);
+    
+    int valPos = 0;
+    for (int i = 0; i < numDims; i++) {
+        if (valDims[i] != 1) {
+            valPos += ( (i / dimCntr[i]) % dims[i] ) * valDimsCntr[i];
+        }
+    }
+    arr[id] = exp(arr[id] - vals[valPos]);
+}
+
+__kernel void div(__global float *arr, __global const size_t *dims,
+                  __global const float *vals, __global const size_t *valDims,
+                  __global const size_t *dimCntr, __global const size_t *valDimsCntr,
+                  int numDims) {
+    int id = get_global_id(0);
+    
+    int valPos = 0;
+    for (int i = 0; i < numDims; i++) {
+        if (valDims[i] != 1) {
+            valPos += ( (i / dimCntr[i]) % dims[i] ) * valDimsCntr[i];
+        }
+    }
+    arr[id] = arr[id] / vals[valPos];
+}
